2f6a4c65c403a8b58237dcc7f12ea8cfd5c73a97
Niran <niranjansaha1081@gmail.com>
2023-03-07 16:36:03 +0530

    basic java code

--- src/main/java/com/example/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/example/learnspringframework/AppGamingBasicJava.java
@@ -2,15 +2,17 @@ package com.example.learnspringframework;
 
 import com.example.learnspringframework.game.GameRunner;
 import com.example.learnspringframework.game.MarioGame;
+import com.example.learnspringframework.game.PacmanGame;
 import com.example.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//var marioGame = new MarioGame();
-		var superContraGame = new SuperContraGame();
-		var gameRunner = new GameRunner(superContraGame);
+//		var game = new MarioGame();
+		var game = new PacmanGame();
+//		var game = new SuperContraGame();
+		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
 
